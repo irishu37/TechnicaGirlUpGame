5e18a75187f6d5a3e8ce32ba1d7101c22364da27
gesna <gesna@DESKTOP-J067NJF>
2017-11-04 17:45:49 -0400

    Stuffs
    Some not working though

--- src/questionBubblePopUp.java
+++ src/questionBubblePopUp.java
@@ -1,8 +1,15 @@
 
+import javax.swing.event.ChangeListener;
+
+import gui.InterestTableGUI.ButtonHandler;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -38,6 +45,7 @@ public class questionBubblePopUp extends Application {
 
 		Button button = new Button("Submit");
 		button.relocate(180, 180);
+		button.setOnAction(new ButtonHandler());
 		canvas.getChildren().add(button); 
 		
 		vb.getChildren().add(canvas);
@@ -48,6 +56,7 @@ public class questionBubblePopUp extends Application {
 	
 	public void displayQuestion(int quesNum) {
 		ToggleGroup group = new ToggleGroup(); 
+		String chosenAnswer=""; 
 		Text t = new Text(70, 70, bank.getQuestion(quesNum).getQuestionText());
 		t.setWrappingWidth(175);
 		canvas.getChildren().addAll(t);
@@ -59,6 +68,18 @@ public class questionBubblePopUp extends Application {
 			canvas.getChildren().addAll(t2);
 		}
 		
+		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+                System.out.println(group.selectedToggleProperty().toString());
+            }
+        });
+	}
+	
+	private class ButtonHandler implements EventHandler<ActionEvent> {
+		@Override
+		public void handle(ActionEvent e) { 
+			output.setText(printedOutput(true,false));
+		}
 	}
 
 	public static void main(String[] args) {
